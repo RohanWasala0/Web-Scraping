7debb177c9f2dcf98e14d60ec9401fb570bdafb0
RohanWasala0 <118281554+RohanWasala0@users.noreply.github.com>
2023-12-10 01:08:57 +0530

    commit

--- kabra_scrap.py
+++ kabra_scrap.py
@@ -11,32 +11,32 @@ website = "https://bfi.net.in/wp-content/uploads/2023/kabrapairs/"
 driver = webdriver.Firefox(options= options)
 driver.get(website)
 
-pair_tbody = driver.find_element(By.TAG_NAME, 'tbody')
-WebDriverWait(pair_tbody, 8).until(
+WebDriverWait(driver, 8).until(
     expected_conditions.presence_of_all_elements_located(
-        (By.XPATH, "//a[contains(@class, 'pairNumberLink')]")
+        (By.XPATH, "//tbody[@id='rBody']/tr")
     )
 )
-pair_numbers = pair_tbody.find_elements(By.XPATH, "//a[contains(@class, 'pairNumberLink')]")
+pair_numbers = driver.find_elements(By.XPATH, "//a[contains(@class, 'pairNumberLink')]")
 temp = list(x.text for x in pair_numbers)
 print(temp)
 input_pair_number = input("Enter the players pair number from the above list \n")
 if input_pair_number in temp:
     input_pair_number_index = temp.index(input_pair_number)
     pair_numbers[input_pair_number_index].click()
-    table_element = driver.find_element(By.ID, "tableH")
+    table_element = driver.find_element(By.XPATH, "//div[@id='contentH']/table[@id='tableH']")
     WebDriverWait(table_element, 10).until(
         expected_conditions.presence_of_all_elements_located(
-            (By.XPATH, "//tb[contains(@rowspan='6')]")
+            (By.XPATH, "//tbody[@id='hBody']/tr")
         )
     )
-    table_data = []
-    for row in table_element.find_elements(By.TAG_NAME, "tr"):
-        row_data = []
-        for cell in row.find_elements(By.TAG_NAME, "td"):
-            row_data.append(cell.text)
-    table_data.append(row_data)
-    print(table_data)
+    table_head = table_element.find_element(By.TAG_NAME, 'thead')
+    table_body = table_element.find_element(By.TAG_NAME, 'tbody')
+
+    table_head_elements = table_head.find_elements(By.XPATH, './td[contains(@scope, "col")]')
+    table_body_elements = table_body.find_elements(By.TAG_NAME, 'tr')
+    for m in table_head_elements:
+        print(m.text)
+    print(len(table_body_elements))
 else:
     print("player pair is not in the player pair list")
 
